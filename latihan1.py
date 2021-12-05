1555377e2623f35e94eca4664501aead5501fca7
rezza fauzi <rezzaf205@gmail.com>
2021-12-05 10:31:40 +0700

    Menambahkan file latihan1.py

--- latihan1.py
+++ latihan1.py
@@ -1,50 +1,84 @@
-daftarkontak = {"Nama":"Nomor Telepon"}
-kontak = {'Ari':'081267888', 'Dina':'087677776'}
+from prettytable import PrettyTable
 
-print("="*30)
-print("   Nama     |    Nomor Telepon   ")
-print("="*30)
-print(" # Ari   |   ",kontak['Ari'])
-print(" # Dina  |   ",kontak['Dina'])
-print("="*30)
+tabel =PrettyTable()
 
-# Menampilkan kontak Ari
-print("Menampilkan kontak Ari")
-print("="*30)
-print(" # Ari   |   ",kontak['Ari'])
-print("="*30)
+tabel.field_names = ['URUTAN', 'NAMA', 'NOMOR TELEPON']
 
-#Menambahkan kontak dengan nama Riko dan nomor 087654544
-print("Menambah kontak dengan dana Riko dan Nomor Telepon 087654544")
-kontak['Riko']='087654544'
-print("="*30)
-print("  Riko   |   ",kontak['Riko'])
-print("="*30)
+fitur = {'1': 'TAMBAH DATA',
+         '2': 'UBAH DATA',
+         '3': 'HAPUS DATA',
+         '4': 'TAMPILKAN DATA',
+         '0': 'KELUAR'
+         }
 
-#Mengubah kontak Dina dengan nomor baru 0889999776
-print("Mengubah kontak Dina dengan nomor baru 0889999776")
-kontak['Dina']='0889999776'
-print("="*30)
-print(" # Dina  |   ",kontak['Dina'])
-print("="*30)
-#Menampilkan semua nama
-print("Menampilkan semua nama")
-print("="*30)
-print(kontak.keys())
-print("="*30)
-#Menampilkan semua nomor
-print("Menampilkan semua nomor")
-print("="*30)
-print(kontak.values())
-print("="*30)
-#Menampilkan semua daftar kontak beserta nama dan nomornya
-print("Menampilkan daftar nama dan nomor")
-print("="*30)
-print(kontak.items())
-print("="*30)
-#Menghapus kontak Dina
-print("Hapus kontak Dina")
-kontak.pop('Dina')
-print("="*30)
-print(kontak.items())
-print("="*30)
+Data = {}
+urutan = 0
+
+
+def TambahData():
+    global urutan
+
+    urutan += 1
+    nama = input('MASUKAN NAMA ANDA :')
+    no = str(input('MASUKAN NO TELPON ANDA :'))
+
+    Data.update({'URUTAN': urutan,
+                 'NAMA': nama,
+                 'NOMOR TELEPON': no
+                 })
+
+    tabel.add_row([urutan, nama, no])
+
+
+def LihatData():
+    print(tabel)
+
+
+def Hapus():
+    print(tabel)
+    urutan = input('URUTAN YANG INGIN DIHAPUS ? :')
+    tanya = input('YAKIN INGIN MENGHAPUS DATA ? (Y / T ) :')
+    if tanya == 'Y' or tanya == 'y':
+        tabel.del_row(int(urutan))
+        print('\t[*] HAPUS DATA BERHASIL')
+    elif tanya == 't' or tanya == 'T':
+        main()
+
+
+def Ubah():
+    print(tabel)
+    urutan = input('URUTAN YANG INGIN DIUBAH ?:')
+    tanya = input('YAKIN INGIN MENGUBAH DATA ? ( Y / t )')
+    if tanya == 'Y' or tanya == 'y':
+        tabel.del_row(int(urutan))
+        print('\t[*] MENGUBAH DATA BERHASIL! ')
+    elif tanya == 't' or tanya == 'T':
+        main()
+
+
+def main():
+    print('\t[*] FITUR YANG TERSEDIA')
+    for key, value in fitur.items():
+        print(f'{key}. {value}')
+    print()
+    pilih = str(input('\t[*] FITUR YANG INGIN DIPILIH :'))
+    while pilih != '0':
+        if pilih == '0':
+            break
+        elif pilih == '1':
+            TambahData()
+        elif pilih == '2':
+            Ubah()
+        elif pilih == '3':
+            Hapus()
+        elif pilih == '4':
+            LihatData()
+
+        else:
+            print('KEYWOARD ERROR!')
+        pilih = str(input('\t[*] FITUR YANG INGIN DIPILIH :'))
+    else:
+        print('\t[*] PROGRAM SELESAI !')
+
+
+main()
